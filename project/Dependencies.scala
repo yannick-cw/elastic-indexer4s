b70746e6b4f14cfb897c733d3cc551706e39613c
Scala Steward <me@scala-steward.org>
2020-02-18 13:04:50 +0100

    Update circe-optics to 0.13.0

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -2,7 +2,7 @@ import sbt._
 
 object Dependencies {
   val elastic4sVersion     = "6.7.2"
-  val circeVersion         = "0.11.0"
+  val circeVersion         = "0.13.0"
   val catsVersion          = "1.6.1"
   val akkaVersion          = "2.5.24"
   val elasticsearchVersion = "6.7.2"
