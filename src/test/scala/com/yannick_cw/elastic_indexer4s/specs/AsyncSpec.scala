e835fc65b84ef71ec0478d41bd8d44ee13cb2ffb
Scala Steward <me@scala-steward.org>
2020-06-19 03:16:29 +0200

    Update scalatest to 3.2.0

--- src/test/scala/com/yannick_cw/elastic_indexer4s/specs/AsyncSpec.scala
+++ src/test/scala/com/yannick_cw/elastic_indexer4s/specs/AsyncSpec.scala
@@ -2,6 +2,8 @@ package com.yannick_cw.elastic_indexer4s.specs
 
 import org.scalatest.concurrent.ScalaFutures
 import org.scalatest._
+import org.scalatest.matchers.should.Matchers
+import org.scalatest.wordspec.AsyncWordSpecLike
 
 abstract class AsyncSpec
     extends AsyncWordSpecLike
